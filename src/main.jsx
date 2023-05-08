20301f6bea089cb79fc47d513a726bbc0d6d36c7
Beatriz Machado <beatrizmcoutinho23@gmail.com>
2023-05-08 16:52:58 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Details } from './pages/Details.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Details />
+  </React.StrictMode>,
+)
+/*
+seleciona dentro do documento um elemento chamado 'root', e renderiza/insere(render) elementos que fazem parte da pagina, fazendo a aplicação ser bem perfomatioca
+*/
